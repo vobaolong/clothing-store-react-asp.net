9fde50c207f3f4763a68f00f7775894f0e55a7ac
Phuc Nguyen <pnguyen@netresultsgroup.com>
2026-06-09 09:16:49 +0700

    update

---
+++ frontend/src/components/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as FreeShipIcon } from './FreeShipIcon'
+export { default as ReturnIcon } from './ReturnIcon'
+export { default as PhoneIcon } from './PhoneIcon'
+export { default as LocationIcon } from './LocationIcon'
